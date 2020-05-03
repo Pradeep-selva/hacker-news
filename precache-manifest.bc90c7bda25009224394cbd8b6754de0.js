75adae3b0d4da883f0598c25f44bba6b7c983172
Pradeep-Selva <pradeepselvaraju@gmail.com>
2020-05-03 20:36:46 +0530

    Updates

---
+++ precache-manifest.bc90c7bda25009224394cbd8b6754de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbf803d4f60d3974f275464cf99fc86",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "385cd212bd23a66633d0",
+    "url": "/hacker-news-clone/static/css/main.1abd7fc6.chunk.css"
+  },
+  {
+    "revision": "5386f1d480c7454751c4",
+    "url": "/hacker-news-clone/static/js/2.977a3f3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news-clone/static/js/2.977a3f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385cd212bd23a66633d0",
+    "url": "/hacker-news-clone/static/js/main.9518bf01.chunk.js"
+  },
+  {
+    "revision": "ddb04f08c4a5bc32deac",
+    "url": "/hacker-news-clone/static/js/runtime-main.2796d82d.js"
+  }
+]);
